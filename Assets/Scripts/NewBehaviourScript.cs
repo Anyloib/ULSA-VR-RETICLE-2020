cde7254fe49ca3419cc09b539beca2ee519857d7
Software Especial <software@ulsa.ulsa-noroeste.edu.mx>
2020-01-21 09:44:14 -0700

    Script

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 200f)]
+
+    float rayDistance = 20f;
+    [SerializeField]
+    LayerMask targetLayer;
+
+    void Update()
+    {
+        if(rayHit)
+        {
+            Debug.Log("target touched");
+        }
+    }
+
+    bool rayHit
+    {
+        get => Physics.RayCast(transform.position, transform.forward, rayDistance, targetLayer);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, transform.forward * rayDistance);
+    }
+}
